ce442d7d2186b81c41ba56513ec37b6cb1ddc8ea
Jean Cesar <neo.jean@gmail.com>
2024-11-21 17:23:25 -0300

    Refatoração do código em server e estruturação do projeto em camadas por responsabilidade

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
 const port = 3000;
 
+routes(app);
+
 app.listen(port, () => {
     console.log(`Servidor escutando na porta ${port}`);
-});
-
-app.get("/api", (request, response) => {
-    response.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
 });
