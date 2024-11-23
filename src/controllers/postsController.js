0167e4478d73852b3ea462d633c19d905eb2e438
Jean Cesar <neo.jean@gmail.com>
2024-11-23 16:17:56 -0300

    Implementacao dos verbos de post e put

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,58 @@
-import { getTodosPosts } from "../models/postsModels.js";
+import fs from "fs";
+import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModels.js";
+import gerarDescricaoComGemini from "../services/geminiServices.js";
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(201).json(postCriado);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ "Error": "Falha na requisição." });
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(201).json(postCriado);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ "Error": "Falha na requisição." });
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImage = `http://localhost:3000/${id}.png`;
+
+    try {
+        const imageBuffer = fs.readFileSync(`uploads/${id}.png`);
+        const descricao = await gerarDescricaoComGemini(imageBuffer);
+
+        const post = {
+            imgUrl: urlImage,
+            descricao: descricao,
+            alt: req.body.alt
+        };
+
+        const postCriado = await atualizarPost(id, post);
+        res.status(204).json(postCriado);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ "Error": "Falha na requisição." });
+    }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import cors from "cors";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
+const upload = multer({ dest: "./uploads" });
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOptions));
 
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost);
 }
 
 export default routes;
